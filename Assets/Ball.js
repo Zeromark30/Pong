9aba3c9a52185b152f39628e62e7cbce7420a56f
deadmarine <kjifcht@gmail.com>
2014-06-30 14:02:41 -0400

    initial commit
    initial commit

---
+++ Assets/Ball.js
@@ -0,0 +1,24 @@
+﻿#pragma strict
+
+var speed : float = 2;
+private var y : float;
+private var dir : Vector2;
+function Start () {
+	rigidbody2D.velocity = Vector2.one.normalized * speed;
+}
+function hitFactor(ballPos : Vector2, racketPos: Vector2, racketHeight : float) : float{
+	return (ballPos.y - racketPos.y) / racketHeight;
+}
+function OnCollisionEnter2D(coll: Collision2D) {
+	if(coll.gameObject.name == "RacketLeft") {
+		y = hitFactor(transform.position, coll.transform.position, 0.64);
+		dir = Vector2(1, y).normalized;
+		rigidbody2D.velocity = dir * speed;
+	}
+	
+	if(coll.gameObject.name == "RacketRight") {
+		y = hitFactor(transform.position, coll.transform.position, 0.64);
+		dir = Vector2(-1, y).normalized;
+		rigidbody2D.velocity = dir * speed;
+	}
+}
---
+++ Assets/MoveRacket.js
@@ -0,0 +1,14 @@
+﻿#pragma strict
+
+var up : KeyCode;
+var down : KeyCode;
+
+function FixedUpdate(){
+	if (Input.GetKey(up)) {
+		transform.Translate(Vector2(0,0.1));
+	}
+	
+	if (Input.GetKey(down)) {
+		transform.Translate(Vector2(0,-0.1));
+	}
+}
